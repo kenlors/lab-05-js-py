dcada27d99c7b7633d47b136e48a78e2a1700015
kenlors <Ken.Lors@mail.citytech.cuny.edu>
2020-10-10 23:11:37 -0400

    made some changes

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+username = input("noun? Adjective? Adverb? Verb?")
+print("On a tropical hamburger
+Underneath a kind moon
+Hanging' gently
+provided questions cause they got all the answers")
+var x = "hamburger"
+var x = "kind"
+var x = "gently"
+var x = "provided"
